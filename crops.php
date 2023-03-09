d6f208fafa877ea347926d4007a4bc729fda58de
nayan-builds <c2020083@hallam.shu.ac.uk>
2023-03-09 11:33:05 +0000

    Added table and styling

--- crops.php
+++ crops.php
@@ -25,6 +25,70 @@
         <header id="title-bar" class="title-bar">
             <h1>Crop types</h1>
         </header>
+        <div class="table-container">
+            <table>
+                <tr>
+                    <th>Crop</th>
+                    <th>PH</th>
+                    <th>Temperature</th>
+                    <th>Humidity</th>
+                    <th>Light</th>
+                    <th>Cost + Maintenance</th>
+                    <th>Yield</th>
+                    <th>Growth Time (Days)</th>
+                </tr>
+                <tr>
+                    <td>Wheat</td>
+                    <td>6-6.8</td>
+                    <td>20-30</td>
+                    <td>40-60</td>
+                    <td>35-65</td>
+                    <td>£100</td>
+                    <td>£450</td>
+                    <td>45</td>
+                </tr>
+                <tr>
+                    <td>Rice</td>
+                    <td>6-6.7</td>
+                    <td>20-27</td>
+                    <td>45-75</td>
+                    <td>50-70</td>
+                    <td>£50</td>
+                    <td>£250</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td>Corn</td>
+                    <td>5.5-7</td>
+                    <td>26-30</td>
+                    <td>50-80</td>
+                    <td>35-85</td>
+                    <td>£75</td>
+                    <td>£400</td>
+                    <td>90</td>
+                </tr>
+                <tr>
+                    <td>Barley</td>
+                    <td>6.5-7</td>
+                    <td>14-20</td>
+                    <td>30-60</td>
+                    <td>20-50</td>
+                    <td>£150</td>
+                    <td>£500</td>
+                    <td>60</td>
+                </tr>
+                <tr>
+                    <td>Oats</td>
+                    <td>6.2-6.6</td>
+                    <td>15-25</td>
+                    <td>25-75</td>
+                    <td>10-40</td>
+                    <td>£80</td>
+                    <td>£300</td>
+                    <td>30</td>
+                </tr>
+            </table>
+        </div>
     </main>
 </body>
 </html>
