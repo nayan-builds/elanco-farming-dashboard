f0df1b32ce9e556f6e9399d341caf99bd798f18e
nayan-builds <c2020083@hallam.shu.ac.uk>
2023-03-16 10:43:42 +0000

    Added average calculations

--- js/plots.js
+++ js/plots.js
@@ -5,16 +5,30 @@ function fillPlotTable(){
         let url = "https://sampledata.elancoapps.com/data/plot/"+plotid;
         $.getJSON(url, (data)=>{
             //Callback function
+            let averages = {
+                ph: 0,
+                temp: 0,
+                humid: 0,
+                light: 0
+            }
             data.forEach(dateData => {
-                let content = "<tr>";
-                content += "<td><a href=\"plot_info.php?plot_num="+i+"\">"+i+"</a></td>";
-                content += "<td>"+dateData.PH+"</td>";
-                content += "<td>"+dateData.Temp_C+"</td>";
-                content += "<td>"+dateData.AVG_Humidity__+"</td>";
-                content += "<td>"+dateData.AVG_Light__+"</td>";
-                content += "</tr>";
-                table.insertAdjacentHTML("beforeend", content);
+                averages.ph += dateData.PH;
+                averages.temp += dateData.Temp_C;
+                averages.humid += dateData.AVG_Humidity__;
+                averages.light += dateData.AVG_Light__;
             });
+            averages.ph /= data.length;
+            averages.temp /= data.length;
+            averages.humid /= data.length;
+            averages.light /= data.length;
+            let content = "<tr>";
+            content += "<td><a href=\"plot_info.php?plot_num="+i+"\">"+i+"</a></td>";
+            content += "<td>"+averages.ph+"</td>";
+            content += "<td>"+averages.temp+"</td>";
+            content += "<td>"+averages.humid+"</td>";
+            content += "<td>"+averages.light+"</td>";
+            content += "</tr>";
+            table.insertAdjacentHTML("beforeend", content);
         })
     }
 }
