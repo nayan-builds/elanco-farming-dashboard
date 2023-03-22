e73ae5f757cc154d8240d2a7a336e0e7346af261
2022508 <c2022508@hallam.shu.ac.uk>
2023-03-22 14:55:47 +0000

    chart

--- js/plot_info.js
+++ js/plot_info.js
@@ -25,3 +25,97 @@ async function fillTable(plot, startDate, endDate){
     content += "</tr>";
     table.insertAdjacentHTML("beforeend", content);
 }
+
+
+
+async function arrayDataInRange(plot, startDate, endDate){
+    const ctx = document.getElementById('myChart');
+    const data = await getPlotDateRange("plot"+plot, startDate, endDate);
+
+    
+        const date = []
+        const ph = []
+        const temp = []
+        const humid = []
+        const light = []
+
+    
+
+    data.forEach(dateData => {
+        date.push(dateData.Date.value);
+        ph.push(dateData.PH);
+        temp.push(dateData.Temp_C);
+        humid.push(dateData.AVG_Humidity__);
+        light.push(dateData.AVG_Light__);
+    });
+
+
+//for all the y axis lines
+// var phLine = {
+//     label: "pH",
+//     data: ph,
+//     borderColor: 'red'
+// };
+// var temperatureLine = {
+//     label: "temperature",
+//     data: temp,
+//     borderColor: 'blue'
+// };
+// var humidityLine = {
+//     label: "humidity",
+//     data: humid,
+//     borderColor: 'green'
+// };
+// var lightLine = {
+//     label: "light",
+//     data: light,
+//     borderColor: 'yellow'
+// };
+
+
+// var dates = {
+//     labels: date,
+//     datasets: [phLine, temperatureLine, humidityLine, lightLine]
+// };
+
+console.log(date);
+console.log(ph);
+
+
+new Chart(ctx, {
+    type: 'line',
+    // data: dates,
+    data: {
+        labels: date,
+        datasets: [
+            {
+                backgroundColor: 'rgb(0, 255, 0)',
+                data: ph,
+                barThickness: 50
+            }]
+      },
+      
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+      
+
+    // options: {
+    //     scales: {
+    //         x: {
+    //             type: 'time',
+    //             time: {
+    //                 unit: 'day'
+    //             }
+    //         },
+    //         y: {
+    //             beginAtZero: true
+    //         }
+    //     }
+    // }
+});
+}
