eab7348e264178534574420f3f0eb5118ca55a80
nayan-builds <c2020083@hallam.shu.ac.uk>
2023-03-23 09:33:46 +0000

    Editted filter bar

--- plot_info.php
+++ plot_info.php
@@ -23,33 +23,36 @@ $plot_num = $_GET["plot_num"];
         <header id="title-bar" class="title-bar">
             <h1>Plot <?php echo $plot_num; ?></h1>
         </header>
-        <div id="date-filter">
-            <form action="" method="get">
-                <input type="hidden" name="plot_num" value="<?php echo $plot_num?>">
-                <div>
-                    <label for="start-date">Start Date:</label>
-                    <input type="date" id="start-date" name="start_date" min="2022-01-01" max="2022-12-31" value="<?php 
-                    if(isset($_GET["start_date"])){
-                        echo $_GET["start_date"];
-                    }
-                    else{
-                        echo "2022-01-01";
-                    }
-                    ?>">
-                </div>
-                <div>
-                    <label for="end-date">End Date:</label>
-                    <input type="date" id="end-date" name="end_date" min="2022-01-01" max="2022-12-31" value="<?php 
-                    if(isset($_GET["end_date"])){
-                        echo $_GET["end_date"];
-                    }
-                    else{
-                        echo "2022-12-31";
-                    }
-                    ?>">
-                </div>
-                <input type="submit">
-            </form>
+        <div id="filter-bar">
+            <h2>Filter</h2>
+            <div id="date-filter">
+                <form action="" method="get">
+                    <input type="hidden" name="plot_num" value="<?php echo $plot_num?>">
+                    <div>
+                        <label for="start-date">Start Date:</label>
+                        <input type="date" id="start-date" name="start_date" min="2022-01-01" max="2022-12-31" value="<?php
+                        if(isset($_GET["start_date"])){
+                            echo $_GET["start_date"];
+                        }
+                        else{
+                            echo "2022-01-01";
+                        }
+                        ?>">
+                    </div>
+                    <div>
+                        <label for="end-date">End Date:</label>
+                        <input type="date" id="end-date" name="end_date" min="2022-01-01" max="2022-12-31" value="<?php
+                        if(isset($_GET["end_date"])){
+                            echo $_GET["end_date"];
+                        }
+                        else{
+                            echo "2022-12-31";
+                        }
+                        ?>">
+                    </div>
+                    <input type="submit">
+                </form>
+            </div>
         </div>
         <div class="table-container">
             <table id="plot-table">
@@ -79,13 +82,11 @@ $plot_num = $_GET["plot_num"];
 
     </main>
     <script src="js/api.js"></script>
-    <!-- <script src="js/graphs.js"></script> -->
     <script src="js/plot_info.js"></script>
     <script>
         var $_GET = <?php echo json_encode($_GET); ?>;
         var plot = $_GET['plot_num'];
 
-        //Need to get dates from form
         if ($_GET.hasOwnProperty('start_date') && $_GET.hasOwnProperty('end_date')){
             fillTable(plot, $_GET['start_date'], $_GET['end_date']);
             arrayDataInRange(plot, $_GET['start_date'], $_GET['end_date']);
